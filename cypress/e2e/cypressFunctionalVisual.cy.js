107918d2ed98d0920c11e7ba06b0c0424e610308
Matt Jasaitis <5661826+MattyJ10@users.noreply.github.com>
2023-07-19 10:19:38 -0500

    cleaned up visual test case

--- cypress/e2e/cypressFunctionalVisual.cy.js
+++ cypress/e2e/cypressFunctionalVisual.cy.js
@@ -7,45 +7,29 @@ describe('Functionally Test Login', () => {
     })
 
     it('should log into a bank account', () => {
-        cy.visit('https://sandbox.applitools.com/bank?validateForm=true');
+        cy.visit('https://sandbox.applitools.com/bank?validateForm=true&theme=true');
 
         cy.get('#log-in').click();
         // Assert Failure - missing username & pass
-        cy.eyesCheckWindow({
-            tag: "Error shown - missing username & password",
-            target: 'window',
-            fully: true
-        });
+        cy.eyesCheckWindow({ tag: "Error shown - missing username & password" });
 
         cy.get('#username').type('user');
         cy.get('#log-in').click();
         // Assert Failure - missing username
-        cy.eyesCheckWindow({
-            tag: "Error shown - missing username",
-            target: 'window',
-            fully: true
-        });
+        cy.eyesCheckWindow({ tag: "Error shown - missing username" });
 
         cy.get('#username').clear();
         cy.get('#password').type('password')
         cy.get('#log-in').click();
         // Assert Failure - missing password
-        cy.eyesCheckWindow({
-            tag: "Error shown - missing password",
-            target: 'window',
-            fully: true
-        });
+        cy.eyesCheckWindow({ tag: "Error shown - missing password" });
 
         cy.get('#username').type('user');
         cy.get('#log-in').click();
         // Assert successful login
-        cy.eyesCheckWindow({
-            tag: "Success - Dashboard shwon",
-            target: 'window',
-            fully: true
-        });
+        cy.eyesCheckWindow({ tag: "Success - Dashboard shwon" });
     })
-    
+
     afterEach(() => {
         cy.eyesClose()
     })
