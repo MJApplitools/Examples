72bdefe1b6f85cdc967374c193ebbdaff68c0484
mjapplitools <matt.jasaitis@applitools.com>
2023-08-30 12:10:04 -0700

    changed float cypress format

--- cypress/e2e/cypressFloatRegions.cy.js
+++ cypress/e2e/cypressFloatRegions.cy.js
@@ -12,7 +12,13 @@ describe('Test Bank App', () => {
         // Use CSS Selectors to specify match level regions
         cy.eyesCheckWindow({ 
             tag: "Floating Icon",
-            floating: {selector: '.loginForm_loginFormLogo__wweIT > a:nth-child(1) > svg:nth-child(1)', maxUpOffset: 10, maxDownOffset: 10, maxLeftOffset: 250, maxRightOffset: 80}
+            floating: {
+                selector: '.loginForm_loginFormLogo__wweIT > a:nth-child(1) > svg:nth-child(1)', 
+                maxUpOffset: 10, 
+                maxDownOffset: 10, 
+                maxLeftOffset: 250, 
+                maxRightOffset: 80
+            }
         });
     })
 
