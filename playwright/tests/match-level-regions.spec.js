36fd10ac3d1458bb28715b6152017630f7667232
Mattyj10 <5661826+MattyJ10@users.noreply.github.com>
2024-01-10 14:18:10 -0800

    adding playwright regions test

---
+++ playwright/tests/match-level-regions.spec.js
@@ -0,0 +1,25 @@
+import { test } from '@playwright/test';
+import {
+    Eyes,
+    Target
+} from '@applitools/eyes-playwright';
+
+test.describe('ACME Bank', () => {
+    let eyes;
+    test.beforeEach(async ({ page }) => {
+        eyes = new Eyes();
+        await eyes.open(page, 'ACME Bank', 'Bank - Match Level Regions', { width: 1200, height: 1000 });
+    });
+
+    test('Test Bank App', async ({ page }) => {
+        await page.goto('https://sandbox.applitools.com/bank/dashboard?layoutAlgo=true');
+        await eyes.check(
+			Target.window()
+				.layoutRegions('.dashboardOverview_accountBalances__3TUPB', '.dashboardTable_dbTable___R5Du')
+		);
+    });
+        
+    test.afterEach(async () => {
+        await eyes.close();
+    });
+});
